8795045f400e71bbbc091cef5c59f70f6c59f683
swwind <i@sww.moe>
2023-04-27 00:09:27 +0800

    fix: vite plugin import

--- app/vite/index.ts
+++ app/vite/index.ts
@@ -5,26 +5,28 @@ import { parseStory } from "../parser";
 type Options = {
   include?: string;
   exclude?: string;
+
+  deserializeSource?: string;
 };
 
 const virtualId = `@markdown-story`;
-const resolveVirtualId = `\0${virtualId}`;
 
 export function markdownStory(options?: Options): Plugin {
   const include = options?.include ?? "src/**/*.md";
   const exclude = options?.exclude ?? "node_modules/**";
+  const deserializeSource =
+    options?.deserializeSource ?? "markdown-story/parser";
+
   const filter = createFilter(include, exclude);
 
   return {
     name: "markdown-story-plugin",
     resolveId(id) {
       if (id === virtualId) {
-        return resolveVirtualId;
-      }
-    },
-    load(id) {
-      if (id === resolveVirtualId) {
-        return inlineStory;
+        return {
+          id: deserializeSource,
+          external: false,
+        };
       }
     },
     transform(markdown, id) {
@@ -42,62 +44,3 @@ export function markdownStory(options?: Options): Plugin {
     },
   };
 }
-
-const inlineStory = `
-export function deserialize(type, ...action) {
-  let index = 0;
-  const param = () => action[index++] || "";
-
-  switch (type) {
-    case 0:
-      return {
-        type: "background",
-        url: param(),
-        parentAnimation: param(),
-        imageAnimation: param(),
-      };
-    case 1:
-      return {
-        type: "foreground",
-        url: param(),
-        parentAnimation: param(),
-        imageAnimation: param(),
-      };
-    case 2:
-      return {
-        type: "character",
-        url: param(),
-        identity: param(),
-        animation: param(),
-      };
-    case 3:
-      return {
-        type: "remove-character",
-        identity: param(),
-      };
-    case 4:
-      return {
-        type: "sfx",
-        url: param(),
-      };
-    case 5:
-      return {
-        type: "text",
-        text: param(),
-        name: param(),
-        vocal: param(),
-      };
-    case 6:
-      return {
-        type: "select",
-        options: action,
-      };
-    case 7:
-      return {
-        type: "bgm",
-        url: param(),
-      };
-  }
-}
-
-`;
--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,11 @@ import { markdownStory } from "./app/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact(), tsconfigPaths(), markdownStory()],
+  plugins: [
+    preact(),
+    tsconfigPaths(),
+    markdownStory({
+      deserializeSource: "./app/parser/serialize.ts",
+    }),
+  ],
 });
