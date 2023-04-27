54600ff342310e057ab1fb21777fbd58d2d498c5
龗云螭 <70850473+Ling-yunchi@users.noreply.github.com>
2023-04-27 23:16:19 +0800

    ✨ feat: 修改首页，添加暂停菜单 (#2)

--- src/app.tsx
+++ src/app.tsx
@@ -5,15 +5,34 @@ export default function () {
   const start = useSignal(false);
 
   if (start.value) {
-    return <Game />;
+    return <Game start={start} />;
   }
 
   return (
-    <div
-      class="text-white w-full h-full bg-black"
-      onClick={() => (start.value = true)}
-    >
-      <h1 class="text-5vvw">点击任意位置开始</h1>
+    <div class="w-full h-full bg-white">
+      <div class="w-full h-full flex flex-col justify-center items-center gap-10 bg-[url(/src/assets/images/background/bg-2.png)] bg-cover bg-center">
+        <h1 class="text-5vvw text-white">我与纳西妲的禁断之恋</h1>
+        <div class="flex flex-col gap-4 text-2vvw text-white">
+          <button
+            class="bg-black bg-opacity-20 hover:bg-opacity-30 py-2 rounded transition-colors px-8"
+            onClick={() => (start.value = true)}
+          >
+            开始游戏
+          </button>
+          <button
+            class="bg-black bg-opacity-20 hover:bg-opacity-30 py-2 rounded transition-colors px-8"
+            onClick={() => alert("并没有可以读取的存档喵~")}
+          >
+            读取存档
+          </button>
+          <button
+            class="bg-black bg-opacity-20 hover:bg-opacity-30 py-2 rounded transition-colors px-8"
+            onClick={() => alert("并没有可以设置的东西喵~")}
+          >
+            设置
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
